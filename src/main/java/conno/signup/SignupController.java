1a06af39a3d97dbdae39c17e874a97d938209aa6
Anastasia Rubtsova <arub008@gmail.com>
2015-03-28 12:54:46 +0300

    signup+ table

--- src/main/java/conno/signup/SignupController.java
+++ src/main/java/conno/signup/SignupController.java
@@ -17,32 +17,44 @@ public class SignupController {
 
     private static final String SIGNUP_VIEW_NAME = "signup/signup";
 
-	@Autowired
-	private AccountRepository accountRepository;
-	
-	@Autowired
-	private UserService userService;
-	
-	@RequestMapping(value = "signup")
-	public String signup(Model model) {
-		model.addAttribute(new SignupForm());
-        return SIGNUP_VIEW_NAME;
-	}
-	
-	@RequestMapping(value = "signup", method = RequestMethod.POST)
-	public String signup(@Valid @ModelAttribute SignupForm signupForm, Errors errors, RedirectAttributes ra) {
-		if (errors.hasErrors()) {
-			return SIGNUP_VIEW_NAME;
-		}
-		Account account = accountRepository.save(signupForm.createAccount());
-		userService.signin(account);
-        // see /WEB-INF/i18n/messages.properties and /WEB-INF/views/homeSignedIn.html
-        MessageHelper.addSuccessAttribute(ra, "signup.success");
-		return "redirect:/login";
-	}
+    @Autowired
+    private AccountRepository accountRepository;
 
-	/*@RequestMapping (value = "homeSignedIn1")
-	public String homeIn(){
-		return "homeSignedIn1";
-	}*/
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "signup")
+    public String signup(Model model) {
+        model.addAttribute(new SignupForm());
+        return SIGNUP_VIEW_NAME;
+    }
+
+    @RequestMapping(value = "signup", method = RequestMethod.POST)
+    public String signup(@Valid @ModelAttribute SignupForm signupForm, Errors errors, RedirectAttributes ra) {
+        if (errors.hasErrors()) {
+            return SIGNUP_VIEW_NAME;
+        }
+        //проверка, совпадают ли пароли
+        if (!signupForm.passwordCheck()) {
+            errors.rejectValue("retypePassword", "password.message");
+            return SIGNUP_VIEW_NAME;
+        }
+
+        //пользователь с таким емайл уже существует и переход на стр
+
+        Account account = accountRepository.findByEmail(signupForm.getEmail());
+        if (account != null) {
+            errors.rejectValue("email", "exist.email");
+            return SIGNUP_VIEW_NAME;
+        }
+
+        account = accountRepository.save(signupForm.createAccount());
+        userService.signin(account);
+        // see /WEB-INF/i18n/messages.properties and /WEB-INF/views/login.html
+
+        MessageHelper.addSuccessAttribute(ra, "signup.success");
+        return "redirect:/login";
+    }
+
+
 }
--- src/main/java/conno/signup/SignupForm.java
+++ src/main/java/conno/signup/SignupForm.java
@@ -9,6 +9,7 @@ public class SignupForm {
 	private static final String NOT_BLANK_MESSAGE = "{notBlank.message}";
 	private static final String EMAIL_MESSAGE = "{email.message}";
 
+
     @NotBlank(message = SignupForm.NOT_BLANK_MESSAGE)
 	@Email(message = SignupForm.EMAIL_MESSAGE)
 	private String email;
@@ -16,7 +17,47 @@ public class SignupForm {
     @NotBlank(message = SignupForm.NOT_BLANK_MESSAGE)
 	private String password;
 
-    public String getEmail() {
+
+	private String retypePassword;
+
+	private String surname;
+	private String name;
+	private String middlename;
+
+
+	public String getRetypePassword() {
+		return retypePassword;
+	}
+
+	public void setRetypePassword(String retypePassword) {
+		this.retypePassword = retypePassword;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMiddlename() {
+		return middlename;
+	}
+
+	public void setMiddlename(String middlename) {
+		this.middlename = middlename;
+	}
+
+	public String getEmail() {
 		return email;
 	}
 
@@ -32,7 +73,11 @@ public class SignupForm {
 		this.password = password;
 	}
 
+	public boolean passwordCheck() {
+		return password.equals(retypePassword);
+	}
+
 	public Account createAccount() {
-        return new Account(getEmail(), getPassword(), "ROLE_USER");
+		return new Account(getEmail(), getPassword(), getSurname(), getName(), getMiddlename(), "ROLE_USER");
 	}
 }
